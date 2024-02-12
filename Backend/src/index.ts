c7f9deacaea39bac5f7e95e5d3b5f202bf33363b
gautam <gautamsajwan8126@gmail.com>
2024-02-12 23:49:50 +0530

    fourth commit

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -9,7 +9,10 @@ const port = process.env.PORT || 5000
 const app = express()
 
 // middlewares
-app.use(cors())
+app.use(cors({
+    origin: [process.env.CORS_ORIGIN],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+}))
 app.use(express.json({limit: '50mb'}))
 app.use('/dalle', dalleRoutes)
 app.use('/post', postRoutes)
