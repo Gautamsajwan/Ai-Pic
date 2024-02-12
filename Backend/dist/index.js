df9eaf195a5609dcfc1e1663680480bce593e53d
gautam <gautamsajwan8126@gmail.com>
2024-02-12 23:59:30 +0530

    fifth commit

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -7,7 +7,10 @@ import postRoutes from './routes/postRoutes.js';
 const port = process.env.PORT || 5000;
 const app = express();
 // middlewares
-app.use(cors());
+app.use(cors({
+    origin: [process.env.CORS_ORIGIN],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use('/dalle', dalleRoutes);
 app.use('/post', postRoutes);
